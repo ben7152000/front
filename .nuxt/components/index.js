f4b724ed245b9e09789f4a01c531fe79edd5e5c2
Ben Chou <ben7152000@gmail.com>
2021-05-01 00:09:04 +0800

    delpoy

---
+++ .nuxt/components/index.js
@@ -0,0 +1,57 @@
+export { default as AboutOne } from '../../components/AboutOne.vue'
+export { default as AboutTwo } from '../../components/AboutTwo.vue'
+export { default as Breadcrumb } from '../../components/Breadcrumb.vue'
+export { default as CallToAction } from '../../components/CallToAction.vue'
+export { default as CallToActionCard } from '../../components/CallToActionCard.vue'
+export { default as ContactForm } from '../../components/ContactForm.vue'
+export { default as Copyright } from '../../components/Copyright.vue'
+export { default as DivingApply } from '../../components/DivingApply.vue'
+export { default as DivingDetail } from '../../components/DivingDetail.vue'
+export { default as DivingDetailCard } from '../../components/DivingDetailCard.vue'
+export { default as DivingGallery } from '../../components/DivingGallery.vue'
+export { default as EquipmentDetail } from '../../components/EquipmentDetail.vue'
+export { default as EquipmentDetailCard } from '../../components/EquipmentDetailCard.vue'
+export { default as FeatureCard } from '../../components/FeatureCard.vue'
+export { default as FeatureCourse } from '../../components/FeatureCourse.vue'
+export { default as FeatureEquipment } from '../../components/FeatureEquipment.vue'
+export { default as FooterNavigation } from '../../components/FooterNavigation.vue'
+export { default as GoogleMap } from '../../components/GoogleMap.vue'
+export { default as HeroSlider } from '../../components/HeroSlider.vue'
+export { default as Loading } from '../../components/Loading.vue'
+export { default as MobileNavigation } from '../../components/MobileNavigation.vue'
+export { default as Navigation } from '../../components/Navigation.vue'
+export { default as OffCanvasMobileMenu } from '../../components/OffCanvasMobileMenu.vue'
+export { default as SectionTitle } from '../../components/SectionTitle.vue'
+export { default as ShapeWithAnimation } from '../../components/ShapeWithAnimation.vue'
+export { default as SocialMedia } from '../../components/SocialMedia.vue'
+export { default as TheFooter } from '../../components/TheFooter.vue'
+export { default as TheHeader } from '../../components/TheHeader.vue'
+
+export const LazyAboutOne = import('../../components/AboutOne.vue' /* webpackChunkName: "components/AboutOne" */).then(c => c.default || c)
+export const LazyAboutTwo = import('../../components/AboutTwo.vue' /* webpackChunkName: "components/AboutTwo" */).then(c => c.default || c)
+export const LazyBreadcrumb = import('../../components/Breadcrumb.vue' /* webpackChunkName: "components/Breadcrumb" */).then(c => c.default || c)
+export const LazyCallToAction = import('../../components/CallToAction.vue' /* webpackChunkName: "components/CallToAction" */).then(c => c.default || c)
+export const LazyCallToActionCard = import('../../components/CallToActionCard.vue' /* webpackChunkName: "components/CallToActionCard" */).then(c => c.default || c)
+export const LazyContactForm = import('../../components/ContactForm.vue' /* webpackChunkName: "components/ContactForm" */).then(c => c.default || c)
+export const LazyCopyright = import('../../components/Copyright.vue' /* webpackChunkName: "components/Copyright" */).then(c => c.default || c)
+export const LazyDivingApply = import('../../components/DivingApply.vue' /* webpackChunkName: "components/DivingApply" */).then(c => c.default || c)
+export const LazyDivingDetail = import('../../components/DivingDetail.vue' /* webpackChunkName: "components/DivingDetail" */).then(c => c.default || c)
+export const LazyDivingDetailCard = import('../../components/DivingDetailCard.vue' /* webpackChunkName: "components/DivingDetailCard" */).then(c => c.default || c)
+export const LazyDivingGallery = import('../../components/DivingGallery.vue' /* webpackChunkName: "components/DivingGallery" */).then(c => c.default || c)
+export const LazyEquipmentDetail = import('../../components/EquipmentDetail.vue' /* webpackChunkName: "components/EquipmentDetail" */).then(c => c.default || c)
+export const LazyEquipmentDetailCard = import('../../components/EquipmentDetailCard.vue' /* webpackChunkName: "components/EquipmentDetailCard" */).then(c => c.default || c)
+export const LazyFeatureCard = import('../../components/FeatureCard.vue' /* webpackChunkName: "components/FeatureCard" */).then(c => c.default || c)
+export const LazyFeatureCourse = import('../../components/FeatureCourse.vue' /* webpackChunkName: "components/FeatureCourse" */).then(c => c.default || c)
+export const LazyFeatureEquipment = import('../../components/FeatureEquipment.vue' /* webpackChunkName: "components/FeatureEquipment" */).then(c => c.default || c)
+export const LazyFooterNavigation = import('../../components/FooterNavigation.vue' /* webpackChunkName: "components/FooterNavigation" */).then(c => c.default || c)
+export const LazyGoogleMap = import('../../components/GoogleMap.vue' /* webpackChunkName: "components/GoogleMap" */).then(c => c.default || c)
+export const LazyHeroSlider = import('../../components/HeroSlider.vue' /* webpackChunkName: "components/HeroSlider" */).then(c => c.default || c)
+export const LazyLoading = import('../../components/Loading.vue' /* webpackChunkName: "components/Loading" */).then(c => c.default || c)
+export const LazyMobileNavigation = import('../../components/MobileNavigation.vue' /* webpackChunkName: "components/MobileNavigation" */).then(c => c.default || c)
+export const LazyNavigation = import('../../components/Navigation.vue' /* webpackChunkName: "components/Navigation" */).then(c => c.default || c)
+export const LazyOffCanvasMobileMenu = import('../../components/OffCanvasMobileMenu.vue' /* webpackChunkName: "components/OffCanvasMobileMenu" */).then(c => c.default || c)
+export const LazySectionTitle = import('../../components/SectionTitle.vue' /* webpackChunkName: "components/SectionTitle" */).then(c => c.default || c)
+export const LazyShapeWithAnimation = import('../../components/ShapeWithAnimation.vue' /* webpackChunkName: "components/ShapeWithAnimation" */).then(c => c.default || c)
+export const LazySocialMedia = import('../../components/SocialMedia.vue' /* webpackChunkName: "components/SocialMedia" */).then(c => c.default || c)
+export const LazyTheFooter = import('../../components/TheFooter.vue' /* webpackChunkName: "components/TheFooter" */).then(c => c.default || c)
+export const LazyTheHeader = import('../../components/TheHeader.vue' /* webpackChunkName: "components/TheHeader" */).then(c => c.default || c)
---
+++ .nuxt/router.js
@@ -0,0 +1,91 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _09f73b5d = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
+const _2ac243cb = () => interopDefault(import('../pages/contact-us.vue' /* webpackChunkName: "pages/contact-us" */))
+const _76c2907b = () => interopDefault(import('../pages/course.vue' /* webpackChunkName: "pages/course" */))
+const _838b446a = () => interopDefault(import('../pages/course-aida-1.vue' /* webpackChunkName: "pages/course-aida-1" */))
+const _836f1568 = () => interopDefault(import('../pages/course-aida-2.vue' /* webpackChunkName: "pages/course-aida-2" */))
+const _8352e666 = () => interopDefault(import('../pages/course-aida-3.vue' /* webpackChunkName: "pages/course-aida-3" */))
+const _636678de = () => interopDefault(import('../pages/equipment.vue' /* webpackChunkName: "pages/equipment" */))
+const _c05162c8 = () => interopDefault(import('../pages/equipment-freediving.vue' /* webpackChunkName: "pages/equipment-freediving" */))
+const _ee7ed366 = () => interopDefault(import('../pages/equipment-snorkeling.vue' /* webpackChunkName: "pages/equipment-snorkeling" */))
+const _29af1622 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/about",
+    component: _09f73b5d,
+    name: "about"
+  }, {
+    path: "/contact-us",
+    component: _2ac243cb,
+    name: "contact-us"
+  }, {
+    path: "/course",
+    component: _76c2907b,
+    name: "course"
+  }, {
+    path: "/course-aida-1",
+    component: _838b446a,
+    name: "course-aida-1"
+  }, {
+    path: "/course-aida-2",
+    component: _836f1568,
+    name: "course-aida-2"
+  }, {
+    path: "/course-aida-3",
+    component: _8352e666,
+    name: "course-aida-3"
+  }, {
+    path: "/equipment",
+    component: _636678de,
+    name: "equipment"
+  }, {
+    path: "/equipment-freediving",
+    component: _c05162c8,
+    name: "equipment-freediving"
+  }, {
+    path: "/equipment-snorkeling",
+    component: _ee7ed366,
+    name: "equipment-snorkeling"
+  }, {
+    path: "/",
+    component: _29af1622,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter () {
+  const router = new Router(routerOptions)
+  const resolve = router.resolve.bind(router)
+
+  // encodeURI(decodeURI()) ~> support both encoded and non-encoded urls
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = encodeURI(decodeURI(to))
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
