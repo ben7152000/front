13ec91e2eae3cecc0268875c6ee1f1a5664b77d5
Ben Chou <ben7152000@gmail.com>
2021-05-01 09:26:31 +0800

    delpoy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apps : [{
+    args: 'start',
     name: 'longdong',
-    script: './node_modules/nuxt-start/bin/nuxt-start.js',
+    script: './node_modules/nuxt/bin/nuxt.js',
     instances: 'max',
     exec_mode: 'cluster',
     max_memory_restart: '1G',
